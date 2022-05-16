f0b43da08c18a599ee40ac56659af400e0331cf7
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-16 11:59:53 +0600

    services connected

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("db connected");
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
