3a94234835db36d96d86ec28c10efc28047d019b
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-16 11:37:08 +0600

    mong1 connected

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://dbuser51:jgUKPrYYPysZlPbc@cluster0.cofp3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
